989231b7e3ec16f18fbc048d750e6d99c47a4267
mauosp <mauricio-0911@hotmail.com>
2023-05-13 17:50:04 -0500

    Baseline basic API

---
+++ helloDevOpsDays/index.js
@@ -0,0 +1,13 @@
+﻿module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". ¡Bienvenido a la demo del DevOpsDays Medellín!"
+        : "¡Bienvenido a la demo del DevOpsDays Medellín!";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
